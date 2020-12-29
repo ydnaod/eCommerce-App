1ad3efbfcafe877d96fe973c9a0079373d24402c
ydnaod <65431123+ydnaod@users.noreply.github.com>
2020-12-28 20:17:48 -0500

    add forms to register page

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,9 +1,30 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 export function Register(){
+
+    const [inputs, setInputs] = useState({
+        username: '',
+        email: '',
+        password: ''
+    });
+
+    const handleChange = (e) => {
+        setInputs({...inputs, [e.target.name]: e.target.value});
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
     return (
         <Fragment>
             <p>Register</p>
+            <form>
+                <input type="text" name="username" placeholder="username" value={inputs.username} onChange={handleChange}/>
+                <input type="email" name="email" placeholder="email" value={inputs.email} onChange={handleChange}/>
+                <input type="password" name="password" placeholder="password" value={inputs.password} onChange={handleChange}/>
+                <button>Submit</button>
+            </form>
         </Fragment>
     )
 }
